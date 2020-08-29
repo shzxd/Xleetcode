5c1b827d285856182faf0c15b5710f5b38360b40
shzxd-lab <xiudongde@outlook.com>
2020-08-29 14:42:49 +0800

    Feats: gcd.py 增加减法思路

--- gcd.py
+++ gcd.py
@@ -4,15 +4,23 @@
 # 以除数和余数反复做除法运算，当余数为 0 时，取当前算式除数为最大公约数
 
 def gcd(a: int, b: int) -> int:
-    while b != 0:
-        r = a % b
-        a = b
-        b = r
-    return a
+    # # 辗转相除
+    # while b != 0:
+    #     r = a % b
+    #     a = b
+    #     b = r
+    # return a
+
+    # # Pythonic 写法
     # while b != 0:
     #     a, b = b, a % b
     # return a
 
+    # 一个递归实现的减法版本
+    # 减法视角：https://plumsempy.com/2020/08/24/gcd-and-the-magic-of-subtraction/
+    if a == 0: return b
+    if b == 0: return a
+    return gcd(abs(a-b), min(a,b))
 
 g = gcd(12, 36)
 print(g)
